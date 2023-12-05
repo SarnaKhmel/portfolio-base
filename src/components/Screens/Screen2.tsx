67e94387ea4e714fad0d3de0b01d599c1c144638
sarnakhmel <OlSa@olsa.local>
2023-12-05 15:37:03 +0200

    add screen2 and screen3

--- src/components/Screens/Screen2.tsx
+++ src/components/Screens/Screen2.tsx
@@ -1,9 +1,19 @@
-import styled from "styled-components";
 import { useTranslation, initReactI18next } from "react-i18next";
 import i18n from "i18next";
 import img2 from "./ScreensImages/img2.svg"
 import doodles from "./ScreensImages/doodle-items.svg"
 
+import {
+  ScreenBlock,
+  InfoBlock,
+  InfoHead,
+  InfoText,
+  TitleWhite,
+  TitleGreen,
+  ImgBlock,
+  Doodles,
+  Body
+} from "./Screen2.styled";
 i18n.use(initReactI18next).init({
   resources: {
     en: {
@@ -34,71 +44,12 @@ const Screen2 = () => {
         </InfoText>
       </InfoBlock>
       <ImgBlock>
-        <img src={doodles} />
-        <img src={img2} />
+        <Doodles src={doodles} />
+        <Body src={img2} />
       </ImgBlock>
     </ScreenBlock>
   );
 };
 
-// const ScreenBlock = styled.div`
-//   height: 70vh;
-//   width: 100%;
-//   padding-top: 100px;
-//   display: grid;
-//   grid-template-columns: 2fr 1fr;
-//   grid-template-rows: repeat(1, 1fr);
-//   grid-column-gap: 0px;
-//   grid-row-gap: 0px;
-// `;
-const ScreenBlock = styled.div`
-  width: 100%;
-  display: grid;
-  column-gap: 20px;
-  grid-template-columns: 3fr 2fr;
-  grid-template-rows: repeat(1, 1fr);
-  grid-column-gap: 0px;
-  grid-row-gap: 0px;
-`;
-const InfoBlock = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-align: center;
-  justify-content: center;
-  padding-left: 50px;
-  border: 1px solid red;
-`;
-
-const InfoHead = styled.div`
-  display: flex;
-`;
-const InfoText = styled.div`
-  max-width: 400px;
-  font-size: 18px;
-  font-weight: 500;
-  line-height: 27px;
-  letter-spacing: 0em;
-  text-align: justify;
-  color: rgba(238, 238, 238, 0.75);
-`;
-
-const TitleWhite = styled.div`
-  font-size: 64px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: 76px;
-`;
-const TitleGreen = styled.div`
-  margin-left: 10px;
-  font-size: 64px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: 76px;
-  color: rgba(22, 73, 0, 1);
-`;
-const ImgBlock = styled.div`
-  position: relative;
-  height: 700px;
-`;
 
 export default Screen2;
---
+++ src/components/Screens/Screen3.styled.ts
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+import doodlesBackground from "./ScreensImages/doodle-items-bg.svg"; 
+
+export const ScreenBlock = styled.div`
+  width: 100%;
+  height: 70vh; 
+  background: url(${doodlesBackground}) repeat;
+`;
+
+
+export const FilterBlock = styled.div`
+margin: 30px;
+
+
+`;
+
+export const Title = styled.div`
+  font-size: 64px;
+  font-weight: 700;
+  line-height: 96px;
+  letter-spacing: 0em;
+  text-align: left;
+`;
+
+export const FilterList = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+  flex-wrap: wrap;
+
+`;
+
+export const FilterItem = styled.li`
+
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 27px;
+  letter-spacing: 0em;
+  text-align: center;
+  color: #fff;
+  padding: 10px 32px;
+  border-radius: 30px;
+  background-color: rgba(57, 62, 70, 0.7);
+  cursor: pointer;
+`;
+
+export const ProjectList = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 20px;
+  margin: 30px;
+`;
+
+export const ProjectItem = styled.div`
+border: 1px solid red;
+border-radius: 10px;
+background-color: rgba(57, 62, 70, 0.5);
+width: 200px;
+display: flex;
+flex-direction: column;
+align-items: center;
+
+`;
+
+export const ProjectImage = styled.img`
+  height: 100px;
+  width: 150px;
+  border-radius: 5px;
+  border: none;
+`;
+
+export const ProjectTitle = styled.div``;
+
+export const ProjectSource = styled.a`
+  text-decoration: none;
+  color: #fff;
+  cursor: pointer;
+`;
