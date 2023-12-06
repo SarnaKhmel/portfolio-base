d69061ace38aaa2c959f3aa17cd1e924b2749b23
sarnakhmel <OlSa@olsa.local>
2023-12-06 18:55:05 +0200

    add screen5 - contacts

--- src/components/Screens/Screen5.tsx
+++ src/components/Screens/Screen5.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import img from './ScreensImages/img3.svg'
-import { ScreenBlock, InfoBlock, Title, ImgBlock, Image, ContactsBlock, ContactList, ContactItem } from './Screen5.styled'
+import { ScreenBlock, InfoBlock, Title, ImgBlock, Image, ContactsBlock, ContactList, ContactRow, ContactItem, Link } from './Screen5.styled'
+import { LuLinkedin, LuAtSign, LuInstagram, LuFacebook, LuPhoneForwarded } from "react-icons/lu";
+import { TbBrandTelegram, TbBrandSkype } from "react-icons/tb";
 
 const Screen4: React.FC = () => {
   return (
     <ScreenBlock name="screen4">
-      <ContactsBlock>
 
+      <ContactsBlock>
         <InfoBlock>
           <Title>
             My contacts
@@ -15,13 +17,37 @@ const Screen4: React.FC = () => {
             <Image src={img} />
           </ImgBlock>
         </InfoBlock>
-
         <ContactList>
-          <ContactItem>Linkedin</ContactItem>
-          <ContactItem>Email</ContactItem>
-          <ContactItem>Facebook</ContactItem>
-          <ContactItem>Telegram</ContactItem>
-          <ContactItem>Phone</ContactItem>
+          <ContactRow>
+            <ContactItem>
+              <LuLinkedin size="24" />
+            </ContactItem>
+            <ContactItem>
+              <LuFacebook size="24" />
+            </ContactItem>
+            <ContactItem>
+              <LuInstagram size="24" />
+            </ContactItem>
+          </ContactRow>
+          <ContactItem>
+            <TbBrandTelegram size="24" />
+            @sarnakhmel
+          </ContactItem>
+          <ContactItem>
+            <TbBrandSkype size="24" />
+            olsarnat
+          </ContactItem>
+          <ContactItem>
+            <LuPhoneForwarded size="24" />
+            <Link href="tel:+380-932-073-52">+380-932-073-52</Link>
+          </ContactItem>
+          <ContactItem>
+            <LuAtSign size="24" />
+            <Link href="email:olsarnat@gmail.com">olsarnat@gmail.com</Link>
+          </ContactItem>
+
+
+
         </ContactList>
       </ContactsBlock>
 
